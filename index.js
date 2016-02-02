9a507b2d897721f31d5688a9cde5b898911e8bce
PatrickHalpin <patrickhalpin@patricks-MacBook-Air.local>
2016-02-02 15:45:28 +0000

    created a basic server woth get reqiest handlers

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //my main file
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res)
+{	
+	var o = {
+		message : 'Hello from node (JSON)'
+	};
+	res.type('application/json');
+	var jsonString = JSON.stringify(o);
+	res.send(jsonString);
+});
+
+app.get('/index.html', function(req,res) 
+{	
+	res.type('png');
+	res.send("<html><body><h3>Hello!</h3></body></html>");
+});
+
+app.listen(3000, function()
+{
+	console.log('example app listening to port 3000');
+});
